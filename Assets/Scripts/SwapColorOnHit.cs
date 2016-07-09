f73d344e816c7bc737e429fdc5129693d7975345
hiremos <sunderwolf@hotmail.fr>
2016-07-09 21:56:09 +0200

    puzzle avec deux éléments à toucher +bug lol

---
+++ Assets/Scripts/SwapColorOnHit.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwapColorOnHit : MonoBehaviour {
+
+    public bool isActivate = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<ShotsParameters>().isEnemyShot == false)
+        {
+            if (isActivate)
+            {
+                gameObject.GetComponentInChildren<SpriteRenderer>().color = new Color(1, 1, 1);
+                isActivate = false;
+            } else
+            {
+                gameObject.GetComponentInChildren<SpriteRenderer>().color = new Color(1, 0, 0);
+                isActivate = true;
+            }
+                
+                
+        }
+        
+    }
+}
---
+++ Assets/Scripts/hideWithOtherElementTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hideWithOtherElementTrigger : MonoBehaviour {
+
+    public GameObject trigger1;
+    public GameObject trigger2;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(trigger1.GetComponent<SwapColorOnHit>().isActivate == true && trigger2.GetComponent<SwapColorOnHit>().isActivate == true)
+        {
+            gameObject.SetActive(false);
+            Debug.Log("hide");
+        } else
+        {
+            gameObject.SetActive(true);
+            Debug.Log("visible");
+        }
+
+    }
+}
