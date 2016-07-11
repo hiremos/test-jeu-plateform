12803ce88cfe3ed62c2b7b11724ab5357fde9f12
hiremos <sunderwolf@hotmail.fr>
2016-07-11 19:40:53 +0200

    ajout plateform + debut de téléporter

---
+++ Assets/Scripts/Plateform/MovePlateform.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlateform : MonoBehaviour {
+
+    public Vector2 direction;
+    public int vitesse;
+    public int distanceAParcourir;
+    private Vector2 m_movement;
+
+    private Vector2 posInitial;
+    private Vector2 posMax;
+    //private Vector2 posMin;
+
+    private bool retour = false;
+
+	// Use this for initialization
+	void Start () {
+        posInitial = gameObject.transform.position;
+
+        if(direction.x != 0)
+        {
+            posMax = new Vector2(gameObject.transform.position.x + distanceAParcourir, 0);
+            //posMin = new Vector2(gameObject.transform.position.x - distanceAParcourir, 0);
+        } else
+        {
+            posMax = new Vector2(0, 0);
+        }
+        
+        if(direction.y != 0)
+        {
+            posMax.y = gameObject.transform.position.y + distanceAParcourir;
+            //posMin= new Vector2(gameObject.transform.position.y - distanceAParcourir, 0);
+        }
+
+        Debug.Log(posMax);
+        Debug.Log(posInitial);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (retour) 
+        {
+            m_movement = new Vector2(
+                -vitesse * direction.x,
+                -vitesse * direction.y);
+        } else
+        {
+            m_movement = new Vector2(
+                vitesse * direction.x,
+                vitesse * direction.y);
+        }
+        
+    }
+
+    private void FixedUpdate()
+    {
+        // Application du mouvement
+        if (gameObject.transform.position.x >= posMax.x && gameObject.transform.position.y >= posMax.y)
+        {
+            Debug.Log("gameobject pos y :"+ gameObject.transform.position.y);
+            Debug.Log("posmax y :" + posMax.y);
+            retour = true;
+        }  
+
+        
+        if (gameObject.transform.position.x < posInitial.x || gameObject.transform.position.y < posInitial.y)
+        {
+            Debug.Log("gameobject pos y :" + gameObject.transform.position.y);
+            Debug.Log("posinitial y :" + posInitial.y);
+            retour = false;
+        }
+        GetComponent<Rigidbody2D>().velocity = m_movement;
+    }
+}
---
+++ Assets/Scripts/Plateform/Teleporter.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour {
+
+    public GameObject teleporterSortie;
+    private Vector2 positionTpSortie;
+    public bool passageJoueur = true;
+    public bool passageTir = false;
+    //private Vector2 positionJoueur;
+
+	// Use this for initialization
+	void Start () {
+        positionTpSortie = teleporterSortie.transform.position;
+        positionTpSortie.y += 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(passageTir)
+        {
+            if (other.tag == "Projectile")
+            {
+                other.transform.position = positionTpSortie;
+            }
+        }
+
+        if (passageJoueur)
+        {
+            if (other.tag == "Player")
+                other.transform.position = positionTpSortie;
+        }
+    }
+}
