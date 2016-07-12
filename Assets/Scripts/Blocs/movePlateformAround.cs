e6e6aae6682a5e7ec6e3286ef727d7aa8e835c6c
hiremos <sunderwolf@hotmail.fr>
2016-07-12 15:33:07 +0200

    debu plateform tournante
    genre le toooooooooooout début lol

---
+++ Assets/Scripts/Blocs/movePlateformAround.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movePlateformAround : MonoBehaviour {
+
+    public float distance;
+    public float speed;
+    public GameObject plateform1;
+    public GameObject plateform2;
+    public bool goRight = true;
+
+	// Use this for initialization
+	void Start () {
+        plateform1.transform.position = new Vector2(gameObject.transform.position.x + distance, gameObject.transform.position.y);
+        plateform1.transform.position = new Vector2(gameObject.transform.position.x - distance, gameObject.transform.position.y);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+	}
+}
