16996e475d70a689c998487c8cdd202253f5dc6e
Ethery <pokemon.74@hotmail.fr>
2016-07-13 16:10:41 +0200

    Maj plateformes rotatives

--- Assets/Scripts/Blocs/movePlateformAround.cs
+++ Assets/Scripts/Blocs/movePlateformAround.cs
@@ -3,51 +3,39 @@ using System.Collections;
 
 public class movePlateformAround : MonoBehaviour {
 
-    public Vector2 distance = new Vector2(1, 1);
-    public float speed = 1;
-    public GameObject plateform1;
-    public GameObject plateform2;
+    public Vector2 distance = new Vector2(2, 2);
+    public float speed;
+    public GameObject[] plateforms;
+    
     public bool goRight = true;
-    public double x = 0;
-    private Vector2 m_movement;
+    public float time = 0;
 
     // Use this for initialization
     void Start () {
-        plateform1.transform.position = new Vector2(gameObject.transform.position.x + distance.x, gameObject.transform.position.y);
-        plateform2.transform.position = new Vector2(gameObject.transform.position.x - distance.x, gameObject.transform.position.y);
-
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(time > 6)
+        {
+            time = 0;
+        }
+        else
+        {
+            time += Time.deltaTime * speed;
+        }
+        
+        Debug.Log(time);
 
-        double test = interpolation_cos(0, 1, x);
-        Debug.Log(plateform1.transform.position);
-
-        //plateform1.transform.position = new Vector2((float)test + gameObject.transform.position.x, (float)test - 1 + gameObject.transform.position.y);
-        m_movement = new Vector2(
-            speed * (gameObject.transform.position.x + (float)test),
-            speed * (gameObject.transform.position.y - 1 + (float)test));
+        float angle = (6 / plateforms.Length);
+        for (int i=0;i < plateforms.Length;i++)
+        {
+            if (plateforms[i] != null)
+            {
+                Vector2 p = new Vector2(Mathf.Cos(time + (angle * i)) * distance.x, Mathf.Sin(time + (angle * i)) * distance.y);
+                plateforms[i].transform.localPosition = p;
+            }
+        }
     }
-
-
-        private void FixedUpdate()
-    {
-        // Application du mouvement
-        plateform1.GetComponent<Rigidbody2D>().velocity = m_movement;
-    }
-
-    public double interpolation_lineaire(double a, double b, double x)
-    {
-        return a * (1 - x) + b * x;
-    }
-
-
-    public double interpolation_cos(double a, double b, double x)
-    {
-        double k = (1 - System.Math.Cos(x * System.Math.PI)) / 2;
-        return interpolation_lineaire(a, b, k);
-    }
-
 }
